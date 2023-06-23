14c31b189f5a7ce896cc6d945a94e774a61de51b
Vinícius <vinileardini@gmail.com>
2023-06-23 13:34:35 -0300

    alterações

--- program.js
+++ program.js
@@ -4,15 +4,68 @@ const telaChamado = document.getElementById("tela-chamado");
 
 const telaInventario = document.getElementById("tela-inventario");
 
+const chamado = {
+
+    nome: nome,
+    local: local,
+    data: data,
+    descricao: descricao,
+
+};
+
+const inventario = {
+
+    equipamento: equipamento,
+    numeroSerie: numeroSerie,
+    local: local,
+};
+
+function adicionarInventario(){
+
+}
+
+function removerInventario(){
+
+}
+
+function editarInventario(){
+
+}
+
+
 function mudarTela(){
 
-    if(document.getElementById("tela")=document.getElementById("telaChamado")){
-        document.getElementById("tela-chamado").hidden=false;
+    if(tela=telaInventario){
+
+        
     }
 
     else{
-        document.getElementById("tela-inventario").hidden=false;
+        document.getElementById("tela-inventario");
     }
     
+
+}
+
+function adicionarChamado(){
+
+}
+
+function fecharChamado(){
+
+}
+
+function editarChamado(){
+
+}
+
+function pesquisaNome(){
+
+}
+
+function pesquisaData(){
+
+    
+
 
 }
