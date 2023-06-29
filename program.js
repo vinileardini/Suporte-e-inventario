b9bd59d308ddf282d6e6d409794180bddf91bf70
Vinícius Camargo <vinileardini@gmail.com>
2023-06-28 21:08:11 -0300

    começo do desenvolvimento do javascript

--- program.js
+++ program.js
@@ -1,28 +1,4 @@
-var tela = document.getElementById("grid-tela");
-
-const telaChamado = document.getElementById("tela-chamado");
-
-const telaInventario = document.getElementById("tela-inventario");
-
-const escolhaChamado = document.getElementById("escolha-chamado")
-
-const escolhaInventario = document.getElementById("escolha-inventario");
-
-const chamado = {
-
-    nome: nome,
-    local: local,
-    data: data,
-    descricao: descricao,
-
-};
-
-const inventario = {
-
-    equipamento: equipamento,
-    numeroSerie: numeroSerie,
-    local: local,
-};
+var escolhaChamado = "Chamados"
 
 function adicionarInventario(){
 
@@ -36,12 +12,20 @@ function editarInventario(){
 
 }
 
+function selectFunction(){
 
-function mudarTelaChamado(){
+    if(document.getElementById("select-inv-chamado").value == escolhaChamado)
+    {
+        document.getElementById("grid-tela").style.display="contents";
+        document.getElementById("tela-inventario").style.display = "none";
+    }
+    else{
 
-}
+        document.getElementById("tela-inventario").style.display="contents";
+        document.getElementById("grid-tela").style.display ="none";
+        
+    }
 
-function mudarTelaInventario(){
 
 }
 
