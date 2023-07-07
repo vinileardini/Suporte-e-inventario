f9821360df796b235d8043c863f7bd4aa1b52701
Vinícius <vinileardini@gmail.com>
2023-07-07 13:47:29 -0300

    banco de dados

---
+++ database.sql
@@ -0,0 +1,21 @@
+create database suporteChamado;
+
+use suporteChamado;
+
+create table chamados{
+    IdNumberChamado varchar (20) primary key not null auto_increment,
+    PersonName varchar (30) not null,
+    DateOpen char (8) not null,
+    Adress varchar (50) not null,
+    Descricao varchar (150)
+
+}
+
+create table inventario{
+    Item varchar (50) not null,
+    DatePlaced varchar (11),
+    Adress varchar (50) not null,
+    IdNumberInventario varchar (20) primary key not null,
+
+}
+
